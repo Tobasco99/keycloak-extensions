8311d3603e1b49cb8d99db2f36d6dd330444f040
Tobasco99 <kiel.tobi99@googlemail.com>
2024-09-23 10:38:11 +0200

    update imports

--- account-console-v3/src/main/resources/theme/learning-layers-account/account/src/app/content/learning-layers-account/ManageClientPage.tsx
+++ account-console-v3/src/main/resources/theme/learning-layers-account/account/src/app/content/learning-layers-account/ManageClientPage.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import {AccountServiceContext} from "../../account-service/AccountServiceContext";
 import {ContentPage} from "../ContentPage";
 import {withRouter, RouteComponentProps} from 'react-router-dom';
